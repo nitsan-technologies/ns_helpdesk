b2c6f25a6fa5b42e79c807a45d88f426142d7e89
NITSAN Technologies <sanjay@nitsan.in>
2022-08-09 15:25:06 +0530

    [TASK] Change URL of t3terminal into t3planet

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Helpdesk TYPO3 Extension',
-    'description' => 'An all-in-one TYPO3 Help Desk extension that helps you balance everything your customers need to be happy. Demo: https://demo.t3terminal.com/t3t-extensions/helpdesk You can download PRO version for more-features & free-support at https://t3terminal.com/ns-helpdesk-typo3-extension',
+    'description' => 'An all-in-one TYPO3 Help Desk extension that helps you balance everything your customers need to be happy. Demo: https://demo.t3planet.com/t3t-extensions/helpdesk You can download PRO version for more-features & free-support at https://t3planet.com/ns-helpdesk-typo3-extension',
     'category' => 'plugin',
     'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
