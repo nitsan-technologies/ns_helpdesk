65c179e3332b8caf7dbc0b9efb70f3a61c2069ed
nitsan-technologies <sanjay@nitsantech.com>
2024-02-14 16:58:55 +0530

    [TASK] fixed TYPO3 error

--- Classes/Controller/NsConstantEditorController.php
+++ Classes/Controller/NsConstantEditorController.php
@@ -122,16 +122,12 @@ class NsConstantEditorController extends AbstractTemplateModuleController
         $site = $request->getAttribute('site');
         $constantIncludeTree = $this->treeBuilder->getTreeBySysTemplateRowsAndSite('constants', $sysTemplateRows, $this->losslessTokenizer, $site);
         $constantAstBuilderVisitor = GeneralUtility::makeInstance(IncludeTreeCommentAwareAstBuilderVisitor::class);
-        $this->treeTraverser->resetVisitors();
-        $this->treeTraverser->addVisitor($constantAstBuilderVisitor);
-        $this->treeTraverser->traverse($constantIncludeTree);
+        $this->treeTraverser->traverse($constantIncludeTree, [$constantAstBuilderVisitor]);
         $constantAst = $constantAstBuilderVisitor->getAst();
         $astConstantCommentVisitor = GeneralUtility::makeInstance(AstConstantCommentVisitor::class);
         $currentTemplateFlatConstants = $this->astBuilder->build($this->losslessTokenizer->tokenize($currentTemplateConstants), new RootNode())->flatten();
         $astConstantCommentVisitor->setCurrentTemplateFlatConstants($currentTemplateFlatConstants);
-        $this->astTraverser->resetVisitors();
-        $this->astTraverser->addVisitor($astConstantCommentVisitor);
-        $this->astTraverser->traverse($constantAst);
+        $this->astTraverser->traverse($constantAst, [$astConstantCommentVisitor]);
 
         $constants = $astConstantCommentVisitor->getConstants();
         $categories = $astConstantCommentVisitor->getCategories();
